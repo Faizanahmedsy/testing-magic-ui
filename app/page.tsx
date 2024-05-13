02e6d1ccfeb777ddfd963a716eefb00291a4b457
faizanahmed-devstree <faizanahmed.devstree>
2024-05-13 19:07:36 +0530

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,211 @@
+"use client";
+import DotPattern from "@/components/magicui/dot-pattern";
+import { cn } from "@/lib/utils";
+import React from "react";
 import { Dock, DockIcon } from "@/components/magicui/dock";
-import Image from "next/image";
+import { useRouter } from "next/navigation";
 export type IconProps = React.HTMLAttributes<SVGElement>;
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogClose } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 
-export default function Home() {
+export default function Page() {
+  const router = useRouter();
+  const [openAppsModal, setOpenAppsModal] = React.useState(false);
+  let size = 48;
   return (
-    <div className="relative flex h-[500px] w-full max-w-[32rem] flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Dock
-      </span>
-      <Dock>
-        <DockIcon>
-          <Icons.gitHub className="h-6 w-6" />
-        </DockIcon>
-        <DockIcon>
-          <Icons.googleDrive className="h-6 w-6" />
-        </DockIcon>
-        <DockIcon>
-          <Icons.notion className="h-6 w-6" />
-        </DockIcon>
-        <DockIcon>
-          <Icons.openai className="h-6 w-6" />
-        </DockIcon>
-        <DockIcon>
-          <Icons.whatsapp className="h-6 w-6" />
-        </DockIcon>
-      </Dock>
-      <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
-    </div>
+    <>
+      <div className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white h-screen flex  justify-center items-center flex-col">
+        <p>Faizan&apos;s Den</p>
+
+        <div className="absolute bottom-0">
+          <Dock>
+            {/* <div onClick={() => alert("foo")}> */}
+            {/* </div> */}
+            <DockIcon>
+              <Icons.googleDrive
+                className="h-6 w-6"
+                onClick={() => setOpenAppsModal(true)}
+              />
+            </DockIcon>
+            <DockIcon>
+              <Icons.gitHub
+                className="h-6 w-6"
+                onClick={() =>
+                  router.push("https://apex-customer.devstree.com/my-auto-1")
+                }
+              />
+            </DockIcon>
+            <DockIcon>
+              <Icons.notion className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.openai className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.whatsapp className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.notion className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.openai className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.whatsapp className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.notion className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.openai className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.whatsapp className="h-6 w-6" />
+            </DockIcon>
+            <DockIcon>
+              <Icons.notion className="h-6 w-6" />
+            </DockIcon>
+          </Dock>
+        </div>
+      </div>
+
+      <DotPattern
+        className={cn(
+          "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)] h-full"
+        )}
+      />
+
+      <Dialog open={openAppsModal} onOpenChange={setOpenAppsModal}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Apps</DialogTitle>
+            <DialogDescription>
+              <div className="mt-5">
+                <div className="grid grid-cols-5 gap-6">
+                  <div>
+                    <Instagram size={size} />
+                    <div>Instagram</div>
+                  </div>
+                  <div>
+                    <Facebook size={size} />
+                    <div>Facebook</div>
+                  </div>
+                  <div>
+                    <Linkedin size={size} />
+                    <div>linkedin</div>
+                  </div>
+                  <div>
+                    <Twitter size={size} />
+                    <div>Twitter</div>
+                  </div>
+                  <div>
+                    <Instagram size={size} />
+                    <div>Instagram</div>
+                  </div>
+                  <div>
+                    <Facebook size={size} />
+                    <div>Facebook</div>
+                  </div>
+                  <div>
+                    <Linkedin size={size} />
+                    <div>linkedin</div>
+                  </div>
+                  <div>
+                    <Twitter size={size} />
+                    <div>Twitter</div>
+                  </div>
+                  <div>
+                    <Instagram size={size} />
+                    <div>Instagram</div>
+                  </div>
+                  <div>
+                    <Facebook size={size} />
+                    <div>Facebook</div>
+                  </div>
+                  <div>
+                    <Linkedin size={size} />
+                    <div>linkedin</div>
+                  </div>
+                  <div>
+                    <Twitter size={size} />
+                    <div>Twitter</div>
+                  </div>
+                  <div>
+                    <Instagram size={size} />
+                    <div>Instagram</div>
+                  </div>
+                  <div>
+                    <Facebook size={size} />
+                    <div>Facebook</div>
+                  </div>
+                  <div>
+                    <Linkedin size={size} />
+                    <div>linkedin</div>
+                  </div>
+                  <div>
+                    <Twitter size={size} />
+                    <div>Twitter</div>
+                  </div>
+                  <div>
+                    <Instagram size={size} />
+                    <div>Instagram</div>
+                  </div>
+                  <div>
+                    <Facebook size={size} />
+                    <div>Facebook</div>
+                  </div>
+                  <div>
+                    <Linkedin size={size} />
+                    <div>linkedin</div>
+                  </div>
+                  <div>
+                    <Twitter size={size} />
+                    <div>Twitter</div>
+                  </div>
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+          {/* <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => setOpenAppsModal(false)}
+              >
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter> */}
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
+
 const Icons = {
   gitHub: (props: IconProps) => (
-    <svg viewBox="0 0 438.549 438.549" {...props}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 512 512"
+      {...props}
+    >
       <path
-        fill="currentColor"
-        d="M409.132 114.573c-19.608-33.596-46.205-60.194-79.798-79.8-33.598-19.607-70.277-29.408-110.063-29.408-39.781 0-76.472 9.804-110.063 29.408-33.596 19.605-60.192 46.204-79.8 79.8C9.803 148.168 0 184.854 0 224.63c0 47.78 13.94 90.745 41.827 128.906 27.884 38.164 63.906 64.572 108.063 79.227 5.14.954 8.945.283 11.419-1.996 2.475-2.282 3.711-5.14 3.711-8.562 0-.571-.049-5.708-.144-15.417a2549.81 2549.81 0 01-.144-25.406l-6.567 1.136c-4.187.767-9.469 1.092-15.846 1-6.374-.089-12.991-.757-19.842-1.999-6.854-1.231-13.229-4.086-19.13-8.559-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-.951-2.568-2.098-3.711-3.429-1.142-1.331-1.997-2.663-2.568-3.997-.572-1.335-.098-2.43 1.427-3.289 1.525-.859 4.281-1.276 8.28-1.276l5.708.853c3.807.763 8.516 3.042 14.133 6.851 5.614 3.806 10.229 8.754 13.846 14.842 4.38 7.806 9.657 13.754 15.846 17.847 6.184 4.093 12.419 6.136 18.699 6.136 6.28 0 11.704-.476 16.274-1.423 4.565-.952 8.848-2.383 12.847-4.285 1.713-12.758 6.377-22.559 13.988-29.41-10.848-1.14-20.601-2.857-29.264-5.14-8.658-2.286-17.605-5.996-26.835-11.14-9.235-5.137-16.896-11.516-22.985-19.126-6.09-7.614-11.088-17.61-14.987-29.979-3.901-12.374-5.852-26.648-5.852-42.826 0-23.035 7.52-42.637 22.557-58.817-7.044-17.318-6.379-36.732 1.997-58.24 5.52-1.715 13.706-.428 24.554 3.853 10.85 4.283 18.794 7.952 23.84 10.994 5.046 3.041 9.089 5.618 12.135 7.708 17.705-4.947 35.976-7.421 54.818-7.421s37.117 2.474 54.823 7.421l10.849-6.849c7.419-4.57 16.18-8.758 26.262-12.565 10.088-3.805 17.802-4.853 23.134-3.138 8.562 21.509 9.325 40.922 2.279 58.24 15.036 16.18 22.559 35.787 22.559 58.817 0 16.178-1.958 30.497-5.853 42.966-3.9 12.471-8.941 22.457-15.125 29.979-6.191 7.521-13.901 13.85-23.131 18.986-9.232 5.14-18.182 8.85-26.84 11.136-8.662 2.286-18.415 4.004-29.263 5.146 9.894 8.562 14.842 22.077 14.842 40.539v60.237c0 3.422 1.19 6.279 3.572 8.562 2.379 2.279 6.136 2.95 11.276 1.995 44.163-14.653 80.185-41.062 108.068-79.226 27.88-38.161 41.825-81.126 41.825-128.906-.01-39.771-9.818-76.454-29.414-110.049z"
-      ></path>
+        fill="#7ca0f3"
+        d="M256 32C132.26 32 32 132.26 32 256s100.26 224 224 224s224-100.26 224-224S379.74 32 256 32m-85 321.89a15.48 15.48 0 0 1-13.46 7.65a14.9 14.9 0 0 1-7.86-2.16a15.48 15.48 0 0 1-5.6-21.21l15.29-25.42a8.73 8.73 0 0 1 7.54-4.3h2.26c11.09 0 18.85 6.67 21.11 13.13Zm129.45-50l-100.13.11h-66.55a15.46 15.46 0 0 1-15.51-16.15c.32-8.4 7.65-14.76 16-14.76h48.24l57.19-97.35l-18.52-31.55C217 137 218.85 127.52 226 123a15.57 15.57 0 0 1 21.87 5.17l9.9 16.91h.11l9.91-16.91A15.58 15.58 0 0 1 289.6 123c7.11 4.52 8.94 14 4.74 21.22l-18.52 31.55l-18 30.69l-39.09 66.66v.11h57.61c7.22 0 16.27 3.88 19.93 10.12l.32.65c3.23 5.49 5.06 9.26 5.06 14.75a13.8 13.8 0 0 1-1.17 5.17Zm77.75.11h-27.11v.11l19.82 33.71a15.8 15.8 0 0 1-5.17 21.53a15.53 15.53 0 0 1-8.08 2.27A15.71 15.71 0 0 1 344.2 354l-29.29-49.86l-18.2-31L273.23 233a38.35 38.35 0 0 1-.65-38c4.64-8.19 8.19-10.34 8.19-10.34L333 273h44.91c8.4 0 15.61 6.46 16 14.75A15.65 15.65 0 0 1 378.23 304Z"
+      />
     </svg>
   ),
   notion: (props: IconProps) => (
@@ -65,30 +235,16 @@ const Icons = {
     </svg>
   ),
   googleDrive: (props: IconProps) => (
-    <svg viewBox="0 0 87.3 78" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      {...props}
+    >
       <path
-        d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"
-        fill="#0066da"
-      />
-      <path
-        d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z"
-        fill="#00ac47"
-      />
-      <path
-        d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z"
-        fill="#ea4335"
-      />
-      <path
-        d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z"
-        fill="#00832d"
-      />
-      <path
-        d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z"
-        fill="#2684fc"
-      />
-      <path
-        d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z"
-        fill="#ffba00"
+        fill="currentColor"
+        d="M11.81 2.45a1.5 1.5 0 0 0-2.12 0L7.961 4.175A1.5 1.5 0 0 0 6.497 3h-3a1.5 1.5 0 0 0-1.5 1.5v8c0 .51.255.96.644 1.231c.243.17.54.27.859.27h8a1.5 1.5 0 0 0 1.5-1.5v-3a1.5 1.5 0 0 0-1.125-1.452L13.58 6.34a1.5 1.5 0 0 0 0-2.121zM7.997 6.42L9.577 8h-1.58zm-1 1.58h-4V4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5zm-3.786 4.91A.5.5 0 0 1 3 12.5V9h3.997v4h-3.5a.5.5 0 0 1-.286-.09m4.786.09V9H11.5a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5z"
       />
     </svg>
   ),
